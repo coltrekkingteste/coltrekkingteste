850d1a2d1eb33abb157457d486ac61733a68f4fc
thiagoassis <thiago.assis@codificar.com.br>
2018-06-09 18:14:29 -0300

    teste

--- index.js
+++ index.js
@@ -403,17 +403,11 @@ function criarEventoDB(req, data, connection, callback) {
 	if(req.session.usuarioLogado.Admin) {
 		connection.query('INSERT INTO evento SET ?', data, function(err, rows, fields) {
 			if(!err) {
-				connection.query('UPDATE `pessoa` SET ListaNegra = 2 WHERE ListaNegra = 1', function(err, rows, fields) {		
-					if(!err) {
-						connection.query('UPDATE `pessoa` SET ListaNegra = 0 WHERE ListaNegra = 2', function(err, rows, fields) {		
-							connection.release();
+				connection.query('UPDATE `pessoa` SET ListaNegra = 2 WHERE ListaNegra = 1', function(err, rows, fields) {
+					connection.release();
 		
-							if(!err) {
-								callback(true);
-							} else {
-								callback(false);
-							}
-						});		
+					if(!err) {
+						callback(true);
 					} else {
 						callback(false);
 					}
@@ -422,6 +416,15 @@ function criarEventoDB(req, data, connection, callback) {
 				callback(false);
 			}
 		});
+		connection.query('UPDATE `pessoa` SET ListaNegra = 2 WHERE ListaNegra = 1', function(err, rows, fields) {
+			connection.release();
+
+			if(!err) {
+				callback(true);
+			} else {
+				callback(false);
+			}
+		});	
 	} else {
 		callback(false);
 	}
