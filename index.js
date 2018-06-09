47ae8b01e6aa6ef67044bc59f1743ae44046ff32
thiagoassis <thiago.assis@codificar.com.br>
2018-06-09 18:25:46 -0300

    tudo pronto

--- index.js
+++ index.js
@@ -405,11 +405,17 @@ function criarEventoDB(req, data, connection, callback) {
 			if(!err) {
 				connection.query('UPDATE `pessoa` SET ListaNegra = 0 WHERE ListaNegra = 2', function(err, rows, fields) {
 					if(!err) {
-						connection.query('UPDATE `pessoa` SET ListaNegra = 2 WHERE ListaNegra = 1', function(err, rows, fields) {
-							connection.release();
-				
+						connection.query('UPDATE `pessoa` SET ListaNegra = 2 WHERE ListaNegra = 1', function(err, rows, fields) {				
 							if(!err) {
-								callback(true);
+								connection.query('UPDATE `pessoa-evento` SET listaNegraEvento = 2 WHERE ListaNegra = 1', function(err, rows, fields) {				
+									connection.release();
+		
+									if(!err) {
+										callback(true);
+									} else {
+										callback(false);
+									}
+								});
 							} else {
 								callback(false);
 							}
