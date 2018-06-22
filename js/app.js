630a828ddb46b59b4e4c5f4c86f03e93ff2bdaa7
thiagoassis <thiago.assis@codificar.com.br>
2018-06-22 18:51:56 -0300

    Funcionando gerar pdf lista de inscritos

--- js/app.js
+++ js/app.js
@@ -590,20 +590,9 @@
 		//Gerar lista PDF
 		$scope.gerarPDF = function(idDoEventoParaGerarTabela) {
 			var pdf = new jsPDF('p', 'pt', 'letter');
-			// source can be HTML-formatted string, or a reference
-			// to an actual DOM element from which the text will be scraped.
-			console.log("Id do evento: " + idDoEventoParaGerarTabela);
 			source = $('#'+idDoEventoParaGerarTabela)[0];
-			console.log("source = " + source);
-
-			// we support special element handlers. Register them with jQuery-style 
-			// ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
-			// There is no support for any other type of selectors 
-			// (class, of compound) at this time.
 			specialElementHandlers = {
-				// element with id of "bypass" - jQuery style selector
 				'#bypassme': function (element, renderer) {
-					// true = "handled elsewhere, bypass text extraction"
 					return true
 				}
 			};
@@ -613,24 +602,21 @@
 				left: 40,
 				width: 522
 			};
-			// all coords and widths are in jsPDF instance's declared units
-			// 'inches' in this case
 			pdf.fromHTML(
-			source, // HTML string or DOM elem ref.
-			margins.left, // x coord
-			margins.top, { // y coord
-				'width': margins.width, // max width of content on PDF
+			source,
+			margins.left,
+			margins.top, { 
+				'width': margins.width,
 				'elementHandlers': specialElementHandlers
 			},
 
 			function (dispose) {
-				// dispose: object with X, Y of the last line add to the PDF 
-				//          this allow the insertion of new lines after html
 				pdf.save('Lista de Inscritos.pdf');
 			}, margins);
 		}
 
 
+		
 		//Adicionar usuario na lista negra
 		$scope.adicionarListaNegra = function(id, idevento) {
 			var dataPost = {
